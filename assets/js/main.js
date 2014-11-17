8a97fd2691bd5b05c00831bdf8fbc21d99e9a071
Aigars Dzerviniks <dzerviniks.aigars@outlook.com>
2014-11-17 18:20:28 +0200

    Revert previous changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,6 @@ document.addEventListener('DOMContentLoaded', function() {
     if (window.location.hash === '#main-header') {
       e.preventDefault();
 
-      document.getElementById('main-header').style.width = 0;
-      document.getElementsByTagName('main')[0].style.right = 0;
       window.history.back();
     }
   });
