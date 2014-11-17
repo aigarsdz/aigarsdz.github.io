d61c59e9f28b37cb0dfcccd3e83227f6aca1adbe
Aigars Dzerviniks <dzerviniks.aigars@outlook.com>
2014-11-17 20:19:29 +0200

    Update else condition

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
   document.getElementById('menu-switch').addEventListener('click', function(e) {
     if (e.currentTarget.getAttribute('href') === '#main-header') {
       e.currentTarget.setAttribute('href', '#');
-    } else if (e.currentTarget.getAttribute('href') === '#') {
+    } else {
       e.currentTarget.setAttribute('href', '#main-header');
     }
   });
