e3cfded2cd3eaef9abbf181a7ad9f3b4da1a8db0
Aigars Dzerviniks <dzerviniks.aigars@outlook.com>
2014-11-17 18:06:12 +0200

    Hide sidebar using JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,8 @@ document.addEventListener('DOMContentLoaded', function() {
     if (window.location.hash === '#main-header') {
       e.preventDefault();
 
-      // document.getElementById('main-header').style.width = 0;
-      // document.getElementsByTagName('main')[0].style.right = 0;
+      document.getElementById('main-header').style.width = 0;
+      document.getElementsByTagName('main')[0].style.right = 0;
       window.history.back();
     }
   });
