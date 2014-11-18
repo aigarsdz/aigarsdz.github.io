646745404e46ab741a732bd26367be6fd0794bb6
Aigars Dzerviniks <dzerviniks.aigars@outlook.com>
2014-11-18 12:39:41 +0200

    Use pure css for navigation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,11 @@
-document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById('menu-switch-container').addEventListener('click', function(e) {
-    var hrefValue = document.getElementById('menu-switch').getAttribute('href');
+// document.addEventListener('DOMContentLoaded', function() {
+//   document.getElementById('menu-switch-container').addEventListener('click', function(e) {
+//     var hrefValue = document.getElementById('menu-switch').getAttribute('href');
 
-    if (hrefValue === '#main-header') {
-      e.currentTarget.innerHTML = '<a href="#" id="menu-switch"><i class="fa fa-bars"></i></a>';
-    } else {
-      e.currentTarget.innerHTML = '<a href="#main-header" id="menu-switch"><i class="fa fa-bars"></i></a>';
-    }
-  });
-});
+//     if (hrefValue === '#main-header') {
+//       e.currentTarget.innerHTML = '<a href="#" id="menu-switch"><i class="fa fa-bars"></i></a>';
+//     } else {
+//       e.currentTarget.innerHTML = '<a href="#main-header" id="menu-switch"><i class="fa fa-bars"></i></a>';
+//     }
+//   });
+// });
