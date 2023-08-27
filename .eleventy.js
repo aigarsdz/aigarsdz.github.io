1c5b7e5b3cf60a58c7d8efc6a6417e8263cb0f22
Aigars Dzerviniks <aigarsdz@pm.me>
2023-08-27 22:35:59 +0300

    Adds a robots.txt file.

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,5 @@ module.exports = function (config) {
   config.addPassthroughCopy('CNAME')
   config.addPassthroughCopy('apple-touch-icon.png')
   config.addPassthroughCopy('favicon.ico')
+  config.addPassthroughCopy('robots.txt')
 }
