58747c95092fe81f44c1efceba03d7a2423e753b
Aigars Dzerviniks <aigarsdz@pm.me>
2025-02-03 14:15:05 +0200

    Updates homepage and adds react-scan for the CSV project.

--- react/csv/src/main.jsx
+++ react/csv/src/main.jsx
@@ -1,6 +1,14 @@
+import { scan } from 'react-scan'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './components/app.jsx'
+
+if (import.meta.env.MODE == 'development') {
+  scan({
+    enabled: true,
+    log: true
+  })
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
